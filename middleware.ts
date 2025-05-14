6cfafd6240f57380188f6c9829f252e5f3e0baca
Tigran Simonyan <tigran.simonyan@hostaways.com>
2025-05-14 14:43:19 +0400

    remove code

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,13 @@ import { NextRequest } from 'next/server';
 import createIntlMiddleware from 'next-intl/middleware';
 
 export default async function middleware(request: NextRequest) {
-  const modifiedRequest = new NextRequest(request.url, {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-  });
-
   const handleI18nRouting = createIntlMiddleware({
     locales: ['en', 'fr'],
     defaultLocale: 'fr',
     localePrefix: 'as-needed',
   });
 
-  const response = handleI18nRouting(modifiedRequest)
+  const response = handleI18nRouting(request)
 
   return response;
 }
